b47bbf789b98e469280fe0c02448d07569a5ee60
Mehmet Yiğit Balık <55997609+YigitBalik@users.noreply.github.com>
2020-10-28 19:12:39 +0300

    Add files via upload

---
+++ Week1/Kodlar/kutuphaneler.c
@@ -0,0 +1,11 @@
+#include <stdio.h> //scanf ve printf
+#include <stdlib.h> // EXIT_SUCCESS;
+#include <math.h> //matematiksel islemler
+
+int main(){
+  int sayi_1 = 100;
+  int sayi_2 = 2;
+  printf("%lf\n",sqrt(sayi_1));
+  printf("%lf\n",pow(sayi_2,3));
+  return 0;
+}
